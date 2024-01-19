67216d06a90ded390bfa2ed53fa31cd1fb74e991
Michele Fernandes <michele.fernandes@watchguard.com>
2024-01-19 17:05:12 -0300

    Criando exemplos de classes

---
+++ src/Exemplo.java
@@ -0,0 +1,6 @@
+public class Exemplo {
+    public static void main(String[] args) {
+        System.out.println("java: class, interface, enum, or record expected");
+        //System.out.println("Ola, estou fazendo testes");
+    }
+}
