d6a79466dc7d64a89f8801d2808990d243d612bd
Michele Fernandes <michele.fernandes@watchguard.com>
2024-01-31 09:59:09 -0300

    Criando brnach da alura

---
+++ src/CriarConta.java
@@ -0,0 +1,40 @@
+public class CriarConta {
+
+    public static void main(String[] args) {
+        Conta contaNova = new Conta(46.5,9874,4569);
+//        contaNova.deposita(5000);
+//
+//        contaNova.setAgenciaConta(6536);
+//        System.out.println(contaNova.getAgenciaConta());
+//
+//        System.out.println(contaNova.getSaldo());
+//
+//        contaNova.saca(300);
+//        System.out.println(contaNova.getSaldo());
+//
+//        Conta contaSecundaria = new Conta();
+//        contaSecundaria.deposita(5000);
+//        contaSecundaria.deposita(800);
+//        contaSecundaria.transfere(600, contaNova);
+//
+//        System.out.println(contaSecundaria.getSaldo());
+//        System.out.println(contaNova.getSaldo());
+//
+//
+//        Cliente terceiraConta = new Cliente();
+//        terceiraConta.setNome("Marcos");
+//        terceiraConta.setCpf("*****cpf*****");
+//        terceiraConta.setProfissao("Contador");
+//        System.out.println(terceiraConta.getNome());
+//        System.out.println(terceiraConta.getCpf());
+//        System.out.println(terceiraConta.getProfissao());
+
+        //Outro exemplo de associar o cliente a conta
+//        Cliente novoCliente = new Cliente();
+//        contaNova.titularConta = novoCliente;
+//        System.out.println(contaNova.titularConta.nome);
+//        System.out.println(contaNova.titularConta.cpf);
+//        System.out.println(contaNova.titularConta.profissao);
+
+    }
+}
